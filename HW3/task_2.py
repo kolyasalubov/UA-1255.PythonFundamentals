476bdafce8c00953d848f8b22a812b18015b2226
SkayKvit <nizkoshapka.ua@gmail.com>
2024-08-28 08:22:01 +0300

    Unite in one file

--- HW3/task_2.py
+++ HW3/task_2.py
@@ -1,5 +1,5 @@
 # find the product of the digits of this number
-number = 5329
+number = input("Input four-digit natural number ")
 number = str( number)
 product = int(number[0]) * int(number[1]) * int(number[2]) * int(number[3]) 
 print (product)
@@ -12,3 +12,10 @@ print (sorted_number)
 # in ascending order, you need to sort the numbers included in the given number
 reversed_number = number[::-1]
 print (reversed_number)
+
+#Interchange the values ​​of two variables without using the third variable.
+a = 5
+b = 6
+a, b = b, a
+print (a)
+print (b)
