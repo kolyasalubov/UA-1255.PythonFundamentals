75242ecbacdabbb9524775bb4f7840238b4d1b8f
Borys Hloba <iamborisgloba@gmail.com>
2024-09-03 19:57:36 +0200

    added HW3

---
+++ HW3/VladykaYura/hw2.py
@@ -0,0 +1,21 @@
+
+user_number = input("Enter your number:")
+
+user_number_list = list(map(int, user_number))
+
+user_number_sum = sum(user_number_list)
+print("Number sum :",user_number_sum)
+
+user_number_sorted = sorted(user_number)
+
+print("Number sorted :",user_number_sorted)
+
+user_number_reverse = user_number [::-1]
+print("Number reverse :",user_number_reverse)
+
+
+
+
+
+
+
