2a9a9a1542bb517d722b9a054e137b42bcc84940
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-09-07 22:23:53 +0300

    Added hw4

---
+++ HW4/OleksandrPanchuk/task.py
@@ -0,0 +1,6 @@
+Celsius = int(input("Enter the temperature in Celsius: "))
+Fahrenheit = float((Celsius *9/5) + 32)
+if Celsius <= -273.15:
+    print("Error: Temperature below absolute zero (-273.15°C)")
+else:
+    print(f"{Celsius}°C is equivalent to {Fahrenheit}°F")
