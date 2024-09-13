d9a7d454ae5118bf401ffd366e12df18113d961a
SkayKvit <nizkoshapka.ua@gmail.com>
2024-09-14 02:43:49 +0300

    Complete first task

---
+++ HW6/NyzkoshapkaOleksandr/task_01.py
@@ -0,0 +1,21 @@
+
+even_list = []
+odd_list = []
+other_list = []
+for item in range (11):
+    if item % 2 == 0 :
+        even_list.append(item)
+    elif item % 3 == 0:
+        odd_list.append(item)
+    else : other_list.append(item)
+print( even_list)
+print(odd_list)
+print(other_list)
+
+even_list = [item for item in range (11) if item % 2 == 0]
+odd_list = [item for item in range (11)  if item % 3 == 0]
+other_list = [item for item in range(11) if item % 2 != 0 and item % 3 != 0 ]
+
+print( even_list)
+print(odd_list)
+print(other_list)
