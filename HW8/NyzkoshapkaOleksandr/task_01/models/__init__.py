bcb395b47ba6af0da4df9ee51b837d5bf1898294
SkayKvit <nizkoshapka.ua@gmail.com>
2024-09-18 13:52:14 +0300

    Complete task1

---
+++ HW8/NyzkoshapkaOleksandr/task_01/models/__init__.py
@@ -0,0 +1,3 @@
+from models.admin import *
+from models.user import *
+__all__= ['create_admin', 'create_user']
---
+++ HW8/NyzkoshapkaOleksandr/task_01/utils/__init__.py
@@ -0,0 +1,3 @@
+from utils.formatter import * 
+from utils.logger import *
+__all__ = ["format_string", "log_in_file"]
