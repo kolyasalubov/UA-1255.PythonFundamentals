d6a95d1c0f0b0c655948af4eac32d95dc3b967a4
SkayKvit <nizkoshapka.ua@gmail.com>
2024-09-18 18:28:06 +0300

    Complete task3

---
+++ HW8/NyzkoshapkaOleksandr/task_03/count_area.py
@@ -0,0 +1,9 @@
+
+def Calculates_area_rectangle(a, b):
+    print ("Area of Rectangle: ", a * b)
+
+def Calculates_area_triangle(a, h):
+     print ("Area of Triangle: ",2*a*h)
+
+def Calculates_area_circle(r):
+    print("Area of Circle: ", pi * pow(r,2))
